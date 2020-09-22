8154353a68ce0b02d95c14833b52ad3cad388085
HikaruKondo <mushin.hudoushin@gmail.com>
2020-09-22 16:39:48 +0900

    add gene cell pca funtion

--- gbdt/src/preprocess/functions.py
+++ gbdt/src/preprocess/functions.py
@@ -3,7 +3,7 @@ from .utils import pca, drop_low_variace
 import pandas as pd
 
 
-def gene_cell_split_pca_use_low_drop_row_variace(df, g_n_comp, c_n_comp, threshold):
+def gene_cell_split_pca_use_row_drop_low_variace(df, g_n_comp, c_n_comp, threshold):
     '''
     split gene and cell
     then, pca gene and cell
@@ -18,4 +18,15 @@ def gene_cell_split_pca_use_low_drop_row_variace(df, g_n_comp, c_n_comp, thresho
     df.reset_index(drop=True, inplace=True)
     concat.reset_index(drop=True, inplace=True)
     df = pd.concat([df, concat], axis=1)
+    return drop_low_variace(df, threshold)
+
+
+def gen_cell_concat_pca_drop_variace(df, n_comp, threshold):
+    genes_cells = [col for col in df.columns if col.startswith(("g-", "c-"))]
+    not_gc = [col for col in df.columns if col not in genes_cells]
+
+    genes_cells_pca = pca(df[genes_cells], n_comp, "pca_{}")
+    df.reset_index(drop=True, inplace=True)
+    genes_cells_pca.reset_index(drop=True, inplace=True)
+    df = pd.concat([df[not_gc], genes_cells_pca], axis=1)
     return drop_low_variace(df, threshold)
